8dff0249862862737951cc65539f37c2197558ab
hsvalenz <73091312+hsvalenz@users.noreply.github.com>
2024-10-08 14:42:55 -0600

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1kLrrd7xDo85O3r6BojCyzZtKdnFWNMyK
 """
 
-! pip install streamlit
 
 import streamlit as st
 import pandas as pd
